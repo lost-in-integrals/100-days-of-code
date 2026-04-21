6666995ebb2f6c6a6fa8f899bd1570410f6a364b
lost-in-integrals <mcflo67@gmail.com>
2026-04-21 19:33:54 +0545

    Function

---
+++ Day-08/main.py
@@ -0,0 +1,34 @@
+from art import logo
+
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, encode_or_decode):
+  end_text = ""
+  if encode_or_decode == "decode":
+    shift_amount *= -1
+  for char in start_text:    
+    if not char.isalpha():
+      end_text += char
+    else:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+  print(f"Here's the {encode_or_decode}d result: {end_text}")
+
+end_of_codes = False  
+while not end_of_codes:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, encode_or_decode=direction)
+
+  cont_caesar = input("Do you want to go again? Type 'yes' if you want to go again. Otherwise type 'no': ").lower()
+  if cont_caesar != "yes":
+    end_of_codes = True
+  
+  if shift > len(alphabet):
+    shift %2 != 0
+     
